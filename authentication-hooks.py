d4da2ee5eafee13862f92fd77f88518ad9bffa05
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2020-07-14 21:38:13 +0200

    Simplified authentication-hooks

--- authentication-hooks.py
+++ authentication-hooks.py
@@ -2,42 +2,44 @@ import logging
 import requests
 import re
 import json
-import HTMLParser
 
 keycloakUrl='http://localhost:9080/auth/realms/exam/protocol/openid-connect/';
 clientId ='ui';
 user='user';
 password='test';
 
-def getCode():
-    #Get login page and form action
-    loginUrl = keycloakUrl + "auth?client_id=" + clientId + "&redirect_uri=https%3A%2F%2Flocalhost%2F&response_type=code";
-    loginPage = requests.get(loginUrl, verify = False);
-    action = re.search('action="([^"]*)"', loginPage.content.decode()).group(1);
-    action = HTMLParser.HTMLParser().unescape(action)
-    #Enter username and password to get the code
-    data = {"username": user, "password": password};
-    codeRedirect = requests.post(action, data= data, verify = False, cookies=loginPage.cookies, allow_redirects=False);
-    code = re.search('code=(.*)$', codeRedirect.headers['Location']).group(1);
-    return code;
+def zap_started(zap, target):
+    logging.debug('Try to get access token')
 
-def getAccessToken(code):
-    tokenUrl = keycloakUrl + 'token';
+    # Make Oauth code request
+    loginUrl = keycloakUrl + "auth?client_id=" + clientId + "&redirect_uri=https%3A%2F%2Flocalhost%2F&response_type=code"
+    loginPage = requests.get(loginUrl, verify = False)
+
+    
+    # We got the login page, let's fill out the form, This might be nicer with a HTML-Parser, but it works like this
+    action = re.search('action="([^"]*)"', loginPage.content.decode()).group(1)
+    action = action.replace('&amp;','&');
+    data = {"username": user, "password": password}
+    codeRedirect = requests.post(action, data= data, verify = False, cookies=loginPage.cookies, allow_redirects=False)
+
+    # We got an OWuaht code redirect, let's take out the code
+    code = re.search('code=(.*)$', codeRedirect.headers['Location']).group(1)
+    tokenUrl = keycloakUrl + 'token'
     data = {"grant_type":"authorization_code", "code": code, "redirect_uri": "https://localhost/", "client_id": clientId};
     accessTokenPage = requests.post(tokenUrl, data= data, verify = False, allow_redirects=False);
-    return (json.loads(accessTokenPage.content))['access_token'];
 
-def zap_started(zap, target):
-    accessToken = getAccessToken(getCode());
+    # On the response there is an access token, let's find it
+    accessToken = (json.loads(accessTokenPage.content))['access_token']
 
+    # Put the access token to every ZAP request
+    zap.script.set_global_var(varkey="authHeader", varvalue=accessToken)
     proxyScriptName = 'add_header.js'
     zap.script.load(scriptname=proxyScriptName,
         scripttype='httpsender',
         scriptengine='Oracle Nashorn',
         filename='/zap/wrk/add_header.js',
-        scriptdescription="desc");
-    print('Use access token ' + accessToken + ' -> ' +
-            zap.script.set_global_var(varkey="authHeader", varvalue=accessToken));
+        scriptdescription="desc")
+    logging.info('Authentication finished use token ' + accessToken)
     return zap, target;
 
 def zap_pre_shutdown(zap):
