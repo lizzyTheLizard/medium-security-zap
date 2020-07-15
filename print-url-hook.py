c906faf439729100bef1d5888baeb0bd395affae
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2020-07-15 23:15:23 +0200

    Remove binary mode

--- print-url-hook.py
+++ print-url-hook.py
@@ -1,7 +1,7 @@
 import logging
 
 def zap_pre_shutdown(zap):
-    with open('/zap/wrk/output/urls.txt', mode='wb' ) as f:
+    with open('/zap/wrk/output/urls.txt', mode='w' ) as f:
         for url in zap.core.urls():
             f.write(str(url) + '\n');
 
